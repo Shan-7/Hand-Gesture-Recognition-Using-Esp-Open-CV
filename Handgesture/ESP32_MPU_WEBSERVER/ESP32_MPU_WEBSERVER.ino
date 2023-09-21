d1e5955179c831d450baec707fee85fa03b5e443
Shan-7 <96657335+Shan-7@users.noreply.github.com>
2023-09-21 23:16:28 +0530

    Add files via upload

---
+++ Handgesture/ESP32_MPU_WEBSERVER/ESP32_MPU_WEBSERVER.ino
@@ -0,0 +1,216 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <Arduino_JSON.h>
+#include "SPIFFS.h"
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>  // Include the SSD1306 OLED library
+
+// Replace with your network credentials
+const char* ssid = "Galaxy S";
+const char* password = "Frequency";
+
+// Create AsyncWebServer object on port 80
+AsyncWebServer server(80);
+
+// Create an Event Source on /events
+AsyncEventSource events("/events");
+
+// Json Variable to Hold Sensor Readings
+JSONVar readings;
+
+// Timer variables
+unsigned long lastTime = 0;  
+unsigned long lastTimeTemperature = 0;
+unsigned long lastTimeAcc = 0;
+unsigned long gyroDelay = 10;
+unsigned long temperatureDelay = 1000;
+unsigned long accelerometerDelay = 200;
+
+// Create a sensor object
+Adafruit_MPU6050 mpu;
+
+sensors_event_t a, g, temp;
+
+float gyroX, gyroY, gyroZ;
+float accX, accY, accZ;
+float temperature;
+
+//Gyroscope sensor deviation
+float gyroXerror = 0.07;
+float gyroYerror = 0.03;
+float gyroZerror = 0.01;
+
+// Initialize the OLED display
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define OLED_RESET    -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+// Mutex for protecting OLED display access
+SemaphoreHandle_t oledMutex;
+
+// Init MPU6050
+void initMPU(){
+  if (!mpu.begin()) {
+    Serial.println("Failed to find MPU6050 chip");
+    while (1) {
+      delay(10);
+    }
+  }
+  Serial.println("MPU6050 Found!");
+}
+
+void initSPIFFS() {
+  if (!SPIFFS.begin()) {
+    Serial.println("An error has occurred while mounting SPIFFS");
+  }
+  Serial.println("SPIFFS mounted successfully");
+}
+
+// Initialize WiFi
+void initWiFi() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.println("");
+  Serial.print("Connecting to WiFi...");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(1000);
+  }
+  Serial.println("");
+  Serial.println(WiFi.localIP());
+}
+
+// Initialize the OLED display
+void initOLED() {
+  if(!display.begin(SSD1306_I2C_ADDRESS, OLED_RESET)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;);
+  }
+  display.display();
+  delay(2000);
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0,0);
+  display.println("Gyro Readings:");
+  display.display();
+}
+
+// Function to update the OLED display with gyro and temperature readings
+void updateOLED() {
+  display.clearDisplay();
+  display.setCursor(0, 0);
+  display.print("GyroX: "); display.println(gyroX);
+  display.print("GyroY: "); display.println(gyroY);
+  display.print("GyroZ: "); display.println(gyroZ);
+  display.print("Temperature: "); display.println(temperature);
+  display.display();
+}
+
+String getGyroReadings(){
+  mpu.getEvent(&a, &g, &temp);
+
+  float gyroX_temp = g.gyro.x;
+  if(abs(gyroX_temp) > gyroXerror)  {
+    gyroX += gyroX_temp/50.00;
+  }
+  
+  float gyroY_temp = g.gyro.y;
+  if(abs(gyroY_temp) > gyroYerror) {
+    gyroY += gyroY_temp/70.00;
+  }
+
+  float gyroZ_temp = g.gyro.z;
+  if(abs(gyroZ_temp) > gyroZerror) {
+    gyroZ += gyroZ_temp/90.00;
+  }
+
+  readings["gyroX"] = String(gyroX);
+  readings["gyroY"] = String(gyroY);
+  readings["gyroZ"] = String(gyroZ);
+
+  // Update the OLED display with a mutex to avoid conflicts
+  xSemaphoreTake(oledMutex, portMAX_DELAY);
+  updateOLED();
+  xSemaphoreGive(oledMutex);
+
+  String jsonString = JSON.stringify(readings);
+  return jsonString;
+}
+
+String getAccReadings() {
+  mpu.getEvent(&a, &g, &temp);
+  // Get current acceleration values
+  accX = a.acceleration.x;
+  accY = a.acceleration.y;
+  accZ = a.acceleration.z;
+  readings["accX"] = String(accX);
+  readings["accY"] = String(accY);
+  readings["accZ"] = String(accZ);
+
+  String accString = JSON.stringify (readings);
+  return accString;
+}
+
+String getTemperature(){
+  mpu.getEvent(&a, &g, &temp);
+  temperature = temp.temperature;
+
+  // Update the OLED display with a mutex to avoid conflicts
+  xSemaphoreTake(oledMutex, portMAX_DELAY);
+  updateOLED();
+  xSemaphoreGive(oledMutex);
+
+  return String(temperature);
+}
+
+// Task to handle web server requests
+void webServerTask(void *parameter) {
+  while (true) {
+    server.handleClient();
+    delay(1);
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  initWiFi();
+  initSPIFFS();
+  initMPU();
+  initOLED(); // Initialize the OLED display
+
+  // Create mutex for OLED access
+  oledMutex = xSemaphoreCreateMutex();
+
+  // Handle Web Server
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/index.html", "text/html");
+  });
+
+  server.serveStatic("/", SPIFFS, "/");
+
+  server.on("/reset", HTTP_GET, [](AsyncWebServerRequest *request){
+    gyroX=0;
+    gyroY=0;
+    gyroZ=0;
+    request->send(200, "text/plain", "OK");
+  });
+
+  server.on("/resetX", HTTP_GET, [](AsyncWebServerRequest *request){
+    gyroX=0;
+    request->send(200, "text/plain", "OK");
+  });
+
+  server.on("/resetY", HTTP_GET, [](AsyncWebServerRequest *request){
+    gyroY=0;
+    request->send(200, "text/plain", "OK");
+  });
+
+  server.on("/resetZ", HTTP_GET, [](AsyncWebServerRequest *request){
+    gyroZ=0;
+   
